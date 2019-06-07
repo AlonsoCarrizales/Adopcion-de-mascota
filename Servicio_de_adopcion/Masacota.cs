cb9018dd032ae0daa58d1af5ca5eb1cbe4f58128
AlonsoCarrizales <50603478+AlonsoCarrizales@users.noreply.github.com>
2019-06-07 07:58:11 -0500

    Add files via upload
    Matricula : 12469
    Grupo Tic7a
    materia : Programación de aplicaciones

---
+++ Servicio_de_adopcion/Masacota.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio_de_adopcion
+{
+    class Masacota
+    {
+        public string nombre { get; set; }
+        public string Raza { get; set; }
+        public int Edad { get; set; }
+        public GeneroM Genero { get; set; }
+        public Especie Especie { get; set; }
+        public string color { get; set; }
+        public double Tamaño { get; set; }
+        public double Peso { get; set; }
+    }
+
+   
+}
---
+++ Servicio_de_adopcion/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio_de_adopcion
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Edad { get; set; }
+        public int Telefono { get; set; }
+        public Genero Genero { get; set; }
+
+    }
+
+    
+}
